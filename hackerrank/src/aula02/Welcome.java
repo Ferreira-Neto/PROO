9c8774b7cd49b52b7b0eddee42588ecd29e8ab2b
Vicente-Neto <ferreira.neto@aluno.ifsp.edu.br>
2026-08-31 22:52:21 -0300

    feat:adicionar exercicios da aula02 no hackerrank

---
+++ hackerrank/src/aula02/Welcome.java
@@ -0,0 +1,13 @@
+package aula02;
+
+import java.util.Scanner;
+public class Welcome {
+    public static void main (String [] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+
+
+}
