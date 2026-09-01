15e6f91e4266d3cbd096545020c9073f5b75bd98
Vicente-Neto <ferreira.neto@aluno.ifsp.edu.br>
2026-08-31 22:59:57 -0300

    feat: adicionar exercicio StdinStdout1.java

--- hackerrank/src/aula02/StdinStdout1.java
+++ hackerrank/src/aula02/StdinStdout1.java
@@ -5,17 +5,15 @@ import java.util.Scanner;
 public class StdinStdout1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
 
 
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
 
-        int i = scan.nextInt();
-        double d = scan.nextDouble();
-        scan.nextLine();
-        String s = scan.nextLine();
-
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
 
     }
 }
