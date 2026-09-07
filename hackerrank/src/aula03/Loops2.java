4d8a0daa448ccf857cd82f5bdb762f7f07b7cb5d
Vicente-Neto <ferreira.neto@aluno.ifsp.edu.br>
2026-09-07 20:58:58 -0300

    feat:Adicionar Exercicio Loops1 e Loops2

---
+++ hackerrank/src/aula03/Loops2.java
@@ -0,0 +1,33 @@
+package aula03;
+import java.util.Scanner;
+
+public class Loops2 {
+    public static void main (String[] args ) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int q = scan.nextInt();
+
+
+        for (int i = 0; i < q; i++) {
+
+            long resultado = a;
+            for (int j = 0; j < n; j++) {
+
+                int a = scan.nextInt();
+                int b = scan.nextInt();
+                int n = scan.nextInt();
+
+                long potencia = (long) Math.pow(2, j);
+                resultado = resultado + b * potencia;
+                System.out.print(resultado + " ");
+            }
+            System.out.println();
+        }
+
+        scan.close();
+
+
+    }
+
+}
